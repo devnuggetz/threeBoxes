607d93ea8fe824799b691dbebc37a02a88acfe93
shuvamk <kshuvam564@gmail.com>
2020-10-26 23:49:08 -0700

    Created Three boxes and Customised

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.scss';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { BoxBufferGeometry } from 'three';
 
+
+const SpinningMesh =({position, args, color})=>{
+  const mesh= useRef(null);
+  useFrame(()=>{
+    mesh.current.rotation.x= mesh.current.rotation.y += 0.01
+  })
+  return(
+    <mesh 
+    position={position}
+    ref={mesh}>
+    <boxBufferGeometry attach='geometry' args={args}/>
+    <meshStandardMaterial attach='material' color={color}/>
+  </mesh>
+  )
+}
 function App() {
+  
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Canvas colorManagement
+      camera={{position:[-5,2,10], fov: 60}}
+      >
+        <ambientLight intensity={0.3} />
+        <SpinningMesh 
+        position={[0,1,0]}
+        args={[3,2,1]}
+        color='lightblue'
+        />
+        <SpinningMesh 
+        position={[-2,1,-5]}
+        color='pink'
+        />
+        <SpinningMesh 
+        position={[5,1,-2]}
+        color='pink'
+        /> 
+      </Canvas>
+    </>
   );
 }
 

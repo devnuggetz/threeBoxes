009a2e3498a0494a6075fecbad33618d6b4f21c0
shuvamk <kshuvam564@gmail.com>
2020-10-27 00:05:42 -0700

    Added SoftShadow from Drei

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useRef } from 'react'
 import './App.scss';
 import { Canvas, useFrame } from 'react-three-fiber';
 import { BoxBufferGeometry } from 'three';
+import {softShadows} from 'drei'
 
+softShadows();
 
 const SpinningMesh =({position, args, color})=>{
   const mesh= useRef(null);
@@ -46,7 +48,7 @@ function App() {
           receiveShadow
           rotation={[-Math.PI/2, 0,0]} position={[0,-3,0]}>
             <planeBufferGeometry attach='geometry' args={[100,100]} />
-            <shadowMaterial attach='material' />
+            <shadowMaterial attach='material' opacity={0.3}/>
           </mesh>
         </group>
         <SpinningMesh 

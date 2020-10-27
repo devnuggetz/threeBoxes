4d03d468cf0a6f49015df3ea02cac46c5404301a
shuvamk <kshuvam564@gmail.com>
2020-10-27 00:03:33 -0700

    Added Shadow plane

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const SpinningMesh =({position, args, color})=>{
     mesh.current.rotation.x= mesh.current.rotation.y += 0.01
   })
   return(
-    <mesh 
+    <mesh castShadow
     position={position}
     ref={mesh}>
     <boxBufferGeometry attach='geometry' args={args}/>
@@ -22,11 +22,12 @@ function App() {
   
   return (
     <>
-      <Canvas colorManagement
+      <Canvas colorManagement shadowMap
       camera={{position:[-5,2,10], fov: 60}}
       >
         <ambientLight intensity={0.3} />
         <directionalLight 
+        castShadow
         position={[0,10,0]} 
         intensity={1.5} 
         shadow-mapSize-width={1024}
@@ -39,6 +40,15 @@ function App() {
         />
         <pointLight position={[-10,0,-20]} intensity={0.5} />
         <pointLight position={[0,-10,0]} intensity={0.5} />
+
+        <group>
+          <mesh 
+          receiveShadow
+          rotation={[-Math.PI/2, 0,0]} position={[0,-3,0]}>
+            <planeBufferGeometry attach='geometry' args={[100,100]} />
+            <shadowMaterial attach='material' />
+          </mesh>
+        </group>
         <SpinningMesh 
         position={[0,1,0]}
         args={[3,2,1]}

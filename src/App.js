86c9ed5a57b063dc2f11bdef2654399c4301c308
shuvamk <kshuvam564@gmail.com>
2020-10-26 23:58:04 -0700

    Added the Directional Light and Shadow Camera

--- src/App.js
+++ src/App.js
@@ -26,6 +26,17 @@ function App() {
       camera={{position:[-5,2,10], fov: 60}}
       >
         <ambientLight intensity={0.3} />
+        <directionalLight 
+        position={[0,10,0]} 
+        intensity={1.5} 
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={+10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        />
         <pointLight position={[-10,0,-20]} intensity={0.5} />
         <pointLight position={[0,-10,0]} intensity={0.5} />
         <SpinningMesh 
